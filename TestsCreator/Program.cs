fb2858a942b5eb16a86cf7f2c8cb05f778c9053f
TheAIBot <andreasuranus@hotmail.com>
2019-11-22 06:13:56 +0100

    add test creator

---
+++ TestsCreator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+
+namespace TestsCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startTestNumber = 31;
+            int testCount = 100;
+
+            StringBuilder sBuilder = new StringBuilder();
+
+            Random rand = new Random(892372);
+            for (int i = 0; i < testCount; i++)
+            {
+                Console.WriteLine(i);
+                int[] numbers = new int[rand.Next(20, 51)];
+                for (int z = 0; z < numbers.Length; z++)
+                {
+                    numbers[z] = rand.Next(1, 2000);
+                }
+
+                var result = Assign_5_solution.Program.Solve(numbers);
+
+                string test =
+                    $@"
+        [TestMethod]
+        public void TestMethod{i + startTestNumber}()
+        {{
+            int[] numbers = new int[] {{ {string.Join(", ", numbers)} }};
+
+            var result = Program.Solve(numbers);
+            Assert.AreEqual({result.number}, result.number);
+            Assert.AreEqual({result.newNumber}, result.newNumber);
+        }}";
+                sBuilder.Append(test);
+                sBuilder.AppendLine();
+                sBuilder.AppendLine();
+            }
+
+            Console.WriteLine(sBuilder.ToString());
+        }
+    }
+}
