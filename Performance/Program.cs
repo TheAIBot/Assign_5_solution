0066418f68b1451ff142c684f6cc9969869a814d
TheAIBot <andreasuranus@hotmail.com>
2019-11-23 04:14:10 +0100

    aded performance project

---
+++ Performance/Program.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+
+namespace Performance
+{
+    public class Benchmarker
+    {
+
+        //[Params(50, 200, 500, 1000)]
+        [Params(100, 200, 400)]
+        public int N;
+
+        private int[] Numbers;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            Random rand = new Random(N);
+            Numbers = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                Numbers[i] = rand.Next(1, 20_001);
+            }
+        }
+
+        [Benchmark]
+        public (int, int) Solve() => Assign_5_solution.Program.Solve(Numbers);
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchmarker>();
+        }
+    }
+}
